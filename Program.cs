7dbb8fc413af27b077c3ebbbc64e57397b387451
shrik <jeanmichel.soler1@epsi.fr>
2019-11-14 17:04:58 +0100

    lol

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controle_Soler_Jean_Michel.ORM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace Controle_Soler_Jean_Michel
     {
         static void Main(string[] args)
         {
+            using (var db = new ContextBDD())
+            {
+                var r1 = db.Jeux.Select(o => new { o.Nom, o.Souhait });
+                /*foreach (var item in r1)
+                {
+                    Console.WriteLine(item.Nom);
+                    Console.WriteLine(item.Souhait);
+                }*/
+
+                //var r2 = db.Clients.Select(o => new { o.ClientID, o.Jeux.Nom});
+                /*foreach (var item in r2)
+                {
+                    Console.WriteLine(item.ClientID);
+                    Console.WriteLine(item.Nom);
+                }*/
+            }
         }
     }
 }
